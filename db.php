c545c95ffff3b1dfd97493338b4ef6b4289a5642
luka725 <luka.sheylashvili@gmail.com>
2021-02-09 16:18:58 +0400

    pushing

--- db.php
+++ db.php
@@ -5,6 +5,23 @@
 include 'constants.php';
 include 'submit.php';
 
+function OpenCon()
+ {
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "1234";
+ $db = "example";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+ return $conn;
+ }
+ 
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
+   
+
 
 class DB {
       
